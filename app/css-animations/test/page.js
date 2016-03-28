81865db6a80b0fd026b47890f626da7478479bd0
Tsvetan Raikov <tsvetan.raikov@telerik.com>
2016-03-28 10:39:16 +0300

    fixed visual-states example

--- app/css-animations/test/page.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-function pageLoaded(args) {
-}
-exports.pageLoaded = pageLoaded;
-//# sourceMappingURL=page.js.map
--- app/css-animations/test/page.ts
+++
@@ -1,4 +0,0 @@
-import observable = require("data/observable");
-
-export function pageLoaded(args: observable.EventData) {
-}
